6fbe5bfe61bc72007590e50c5a9ef0aa24695857
Garrett <g.romo2@students.clark.edu>
2020-05-27 12:19:27 -0700

    Lab

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+<Garrett>
+<Mod 6 Lab 1>
 <assignment/lab description
 """
 
@@ -12,6 +12,31 @@ Output: return value and description
 """
 
 def main():
-    # code here
+    '''
+    # definite loop example
+    for i in range(0, 11, 2):
+        print(i)
+
+    # same behavior but coded as indefinite or while loop
+    count = 0
+    while (count < 11):
+        print(count)
+        # count = count + 2
+        count += 2
+    print()
+    
+
+
+    if x and y: 
+        print("Both True")
+    else:
+        print("Other")
+    print()
+    '''
+
+    print(True and False)
+    print(True and True)
+    print(True and False or False)
+    print(False or False or True)
 
 main()    
